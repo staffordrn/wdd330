005123b2a0c92f7ea5f669cd3f7bea602527ab68
staffordrn <sta18036@byui.edu>
2021-05-01 23:50:53 -0400

    week2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-console.log("hello world");
+
 
 function make_list() {
   const links = [
     {
       label: "Week 1 Notes",
       url: "week1/index.html"
+    },
+    {
+      label: "Week 2 Notes",
+      url: "week2/index.html"
     }//, <--dont forget the comma in between array elements
   ]
   links.forEach((link)=> {
@@ -14,3 +18,41 @@ function make_list() {
 }
 
 make_list();
+
+// Quiz Ninja Project 
+function quizNinjaGame() {
+  const quiz = [
+  ["What is Superman's real name?","Clark Kent"],
+  ["What is Wonder Woman's real name?","Diana Prince"],
+  ["What is Batman's real name?","Bruce Wayne"]
+  ];
+  function start(quiz){
+    let score = 0;
+    // main game loop
+    for(const [question,answer] of quiz){
+    const response = ask(question);
+    check(response,answer);
+    }
+    // end of main game loop
+    gameOver();
+    // function declarations
+    function ask(question){
+    return prompt(question);
+    }
+    function check(response,answer){
+    if(response === answer){
+    alert('Correct!');
+    score++;
+    } else {
+    alert(`Wrong! The correct answer was ${answer}`);
+    }
+    }
+    function gameOver(){
+    alert(`Game Over, you scored ${score} point${score !== 1 ?
+    's' : ''}`);
+    }
+  }
+  start(quiz);
+
+}
+
